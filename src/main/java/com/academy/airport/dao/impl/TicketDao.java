0ac14377951ee4d60a88e64d613e1d9de61954d9
Stellmaria <aam.cheshire@gmail.com>
2026-08-19 19:04:47 +0300

    complete ticket DAO CRUD

--- src/main/java/com/academy/airport/dao/impl/TicketDao.java
+++ src/main/java/com/academy/airport/dao/impl/TicketDao.java
@@ -9,37 +9,40 @@ import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public class TicketDao implements Dao<Long, Ticket> {
     private static final TicketDao INSTANCE = new TicketDao();
+
     @Language("PostgreSQL")
-    private static final String DELETE_SQL = "DELETE "
-            + "FROM airport_storage.ticket "
-            + "WHERE id = ?;";
-    private static final String SAVE_SQL = "INSERT INTO airport_storage.ticket(user_id, route_id, seat_no, cost)"
-            + " VALUES (?, ?, ?, ?);";
+    private static final String DELETE_SQL = "DELETE FROM airport_storage.ticket WHERE id = ?;";
+    @Language("PostgreSQL")
+    private static final String SAVE_SQL = "INSERT INTO airport_storage.ticket(user_id, route_id, seat_no, cost) VALUES (?, ?, ?, ?);";
+    @Language("PostgreSQL")
+    private static final String UPDATE_SQL = "UPDATE airport_storage.ticket SET user_id = ?, route_id = ?, seat_no = ?, cost = ? WHERE id = ?;";
+    @Language("PostgreSQL")
+    private static final String FIND_ALL_SQL = "SELECT id, user_id, route_id, seat_no, cost FROM airport_storage.ticket";
+    @Language("PostgreSQL")
+    private static final String FIND_BY_ID_SQL = FIND_ALL_SQL + " WHERE id = ?;";
 
     @Override
     @SneakyThrows
     public Ticket save(final @NotNull Ticket entity) {
         try (var connection = ConnectionManager.get();
-             var prepareStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
-            prepareStatement.setObject(1, entity.getUserId());
-            prepareStatement.setObject(2, entity.getRouteId());
-            prepareStatement.setObject(3, entity.getSeatNo());
-            prepareStatement.setObject(4, entity.getCost());
-
-            prepareStatement.executeUpdate();
-
-            var generatedKeys = prepareStatement.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                entity.setId(generatedKeys.getObject("id", Long.class));
+             var statement = connection.prepareStatement(SAVE_SQL, RETURN_GENERATED_KEYS)) {
+            bindTicket(statement, entity);
+            statement.executeUpdate();
+            try (var generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    entity.setId(generatedKeys.getObject("id", Long.class));
+                }
             }
             return entity;
         }
@@ -47,35 +50,75 @@ public class TicketDao implements Dao<Long, Ticket> {
 
     @Override
     @SneakyThrows
-    public void update(final Ticket entity) {
-
+    public void update(final @NotNull Ticket entity) {
+        try (var connection = ConnectionManager.get();
+             var statement = connection.prepareStatement(UPDATE_SQL)) {
+            bindTicket(statement, entity);
+            statement.setObject(5, entity.getId());
+            statement.executeUpdate();
+        }
     }
 
     @Override
     @SneakyThrows
     public boolean delete(final Long id) {
         try (var connection = ConnectionManager.get();
-             var prepareStatement = connection.prepareStatement(DELETE_SQL)) {
-            prepareStatement.setObject(1, id);
-            return prepareStatement.executeUpdate() > 0;
+             var statement = connection.prepareStatement(DELETE_SQL)) {
+            statement.setObject(1, id);
+            return statement.executeUpdate() > 0;
         }
     }
 
     @Override
     @SneakyThrows
     public Optional<Ticket> findById(final Long id) {
-        return Optional.empty();
+        try (var connection = ConnectionManager.get()) {
+            return findById(id, connection);
+        }
     }
 
     @Override
-    public Optional<Ticket> findById(Long id, Connection connection) {
-        return Optional.empty();
+    @SneakyThrows
+    public Optional<Ticket> findById(final Long id, final Connection connection) {
+        try (var statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            statement.setObject(1, id);
+            try (var resultSet = statement.executeQuery()) {
+                return resultSet.next() ? Optional.of(buildTicket(resultSet)) : Optional.empty();
+            }
+        }
     }
 
     @Override
     @SneakyThrows
     public List<Ticket> findAll() {
-        return null;
+        try (var connection = ConnectionManager.get();
+             var statement = connection.prepareStatement(FIND_ALL_SQL);
+             var resultSet = statement.executeQuery()) {
+            List<Ticket> tickets = new ArrayList<>();
+            while (resultSet.next()) {
+                tickets.add(buildTicket(resultSet));
+            }
+            return tickets;
+        }
+    }
+
+    @SneakyThrows
+    private void bindTicket(final java.sql.PreparedStatement statement, final Ticket entity) {
+        statement.setObject(1, entity.getUserId());
+        statement.setObject(2, entity.getRouteId());
+        statement.setString(3, entity.getSeatNo());
+        statement.setBigDecimal(4, entity.getCost());
+    }
+
+    @SneakyThrows
+    private Ticket buildTicket(final ResultSet resultSet) {
+        return Ticket.builder()
+                .id(resultSet.getObject("id", Long.class))
+                .userId(resultSet.getObject("user_id", Integer.class))
+                .routeId(resultSet.getObject("route_id", Long.class))
+                .seatNo(resultSet.getString("seat_no"))
+                .cost(resultSet.getBigDecimal("cost"))
+                .build();
     }
 
     public static TicketDao getInstance() {
