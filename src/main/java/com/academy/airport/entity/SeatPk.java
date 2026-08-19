8ac998b2899921877480a77a66630c1a45efbb86
Stellmaria <aam.cheshire@gmail.com>
2026-08-19 19:05:01 +0300

    fix seat composite key model

--- src/main/java/com/academy/airport/entity/SeatPk.java
+++ src/main/java/com/academy/airport/entity/SeatPk.java
@@ -1,25 +1,21 @@
 package com.academy.airport.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class SeatPk implements Serializable {
-    @Column(name = "airplane_id", nullable = false)
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer airplaneId;
-    @Column(name = "seat_no", nullable = false, length = 4)
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String seatNo;
 }
